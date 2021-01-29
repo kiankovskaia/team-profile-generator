619b5c6b7453b67d99907210b86d1a0657c9f0ac
yourdilemma <k.iankovskaia@gmail.com>
2021-01-28 23:33:20 -0500

    last changes

--- index.js
+++ index.js
@@ -5,133 +5,146 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const render = require('./lib/htmlRenderer')
+const render = require("./lib/htmlRenderer");
 
 const OUTPUT_DIR = path.resolve(__dirname, "dist");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const engineerQuestions = []
+const engineerQuestions = [];
 let team = [];
 
-function askRole(){
+function askRole() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "Enter Role",
+        choices: ["Engineer", "Manager", "Intern", "Done"],
+      },
+    ])
+    .then((role) => {
+      switch (role.role) {
+        case "Engineer":
+          inquirer
+            .prompt([
+              {
+                name: "name",
+                message: "Enter Name",
+              },
+              {
+                name: "id",
+                message: "Enter Id",
+              },
+              {
+                name: "email",
+                message: "Enter Email",
+              },
+              {
+                name: "github",
+                message: "Enter GitHub url",
+              },
+            ])
+            .then((engineer) => {
+              const eng = new Engineer(
+                engineer.name,
+                engineer.id,
+                engineer.email,
+                engineer.github
+              );
+              console.log("engineer: " + JSON.stringify(eng));
+              console.info(
+                "Name: " + engineer.name,
+                " Id: " + engineer.id,
+                " Email: " + engineer.email,
+                " Github: " + engineer.github
+              );
 
+              team.push(eng);
+              askRole();
+            });
+          break;
+        case "Manager":
+          inquirer
+            .prompt([
+              {
+                name: "name",
+                message: "Enter Name",
+              },
+              {
+                name: "id",
+                message: "Enter Id",
+              },
+              {
+                name: "email",
+                message: "Enter Email",
+              },
+              {
+                name: "officeNumber",
+                message: "Enter office number",
+              },
+            ])
+            .then((manager) => {
+              const man = new Manager(manager.name, manager.id, manager.email, manager.officeNumber);
+              console.log("manager: " + JSON.stringify(man));
+              console.info(
+                "Name: " + manager.name,
+                " Id: " + manager.id,
+                " Email: " + manager.email,
+                " OfficeNumber " + manager.officeNumber
+              );
+              team.push(man);
+              askRole();
+            });
+          break;
+        case "Intern":
+          inquirer
+            .prompt([
+              {
+                name: "name",
+                message: "Enter Name",
+              },
+              {
+                name: "id",
+                message: "Enter Id",
+              },
+              {
+                name: "email",
+                message: "Enter Email",
+              },
+              {
+                name: "school",
+                message: "Enter School",
+              },
+            ])
+            .then((intern) => {
+              const int = new Intern(
+                intern.name,
+                intern.id,
+                intern.email,
+                intern.school
+              );
+              console.log("intern: " + JSON.stringify(int));
+              console.info(
+                "Name: " + intern.name,
+                " Id: " + intern.id,
+                " Email: " + intern.email,
+                " School: " + intern.school
+              );
+              team.push(int);
+              askRole();
+            });
+            break;
+        default:
+          writeToFile();
+          break;
+      }
 
-
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'role',
-      message: 'Enter Role',
-      choices: ['Engineer', 'Manager', 'Intern', 'Done'],
-    }
-
-  ]).then(role => {
-  switch (role.role) {
-    case "Engineer" :
-      inquirer
-      .prompt([
-        {
-          name: 'name',
-          message: 'Enter Name',
-        },
-        {
-          name: 'id',
-          message: 'Enter Id'
-        },
-        {
-          name: 'email',
-          message: "Enter Email"
-        },
-        {
-          name: 'github',
-          message: 'Enter GitHub url'
-        }
-    
-      ]).then(engineer =>{
-       const eng = new Engineer(engineer.name, engineer.id, engineer.email, engineer.github);
-       console.log("engineer: " + JSON.stringify(eng)) 
-       console.info('Name: ' + engineer.name, ' Id: ' + engineer.id, ' Email: ' + engineer.email, ' Github: ' + engineer.github, );
-       
-       
-       team.push(eng)
-       askRole()
-      })
-      break;
-      case "Manager": 
-      inquirer
-        .prompt([
-          {
-            name: "name",
-            message: "Enter Name",
-          },
-          {
-            name: "id",
-            message: "Enter Id",
-          },
-          {
-            name: "email",
-            message: "Enter Email",
-          },
-          {
-            name: "officeNumber",
-            message: "Enter office number",
-          },
-        ])
-        .then((manager) => {
-          const man = new Manager(manager.name, manager.id, manager.email);
-          console.log("manager: " + JSON.stringify(man));
-          console.info(
-            "Name: " + manager.name,
-            " Id: " + manager.id,
-            " Email: " + manager.email
-          );
-          team.push(man)
-          askRole()
-        });
-      break;
-      case "Intern": 
-      inquirer
-      .prompt([
-        {
-          name: 'name',
-          message: 'Enter Name',
-        },
-        {
-          name: 'id',
-          message: 'Enter Id'
-        },
-        {
-          name: 'email',
-          message: "Enter Email"
-        },
-        {
-          name: 'school',
-          message: 'Enter School'
-        } 
-
-    
-      ]).then(intern =>{
-        const int = new Intern(intern.name, intern.id, intern.email, intern.school);
-       console.log("intern: " + JSON.stringify(int))
-        console.info('Name: ' + intern.name, ' Id: ' + intern.id, ' Email: ' + intern.email, ' School: ' + intern.school);
-        team.push(int)
-        askRole()
-      })
-      default:
-        writeToFile()
-        break
-      
-  }
-
-  function writeToFile(){
-    fs.writeFileSync(outputPath, render(team), "utf-8");
-    console.log("SUCCESS!")
-  }
-       
-  
-    
-  });
+      function writeToFile() {
+        fs.writeFileSync(outputPath, render(team), "utf-8");
+        console.log("SUCCESS!");
+      }
+    });
 }
 
 askRole();
